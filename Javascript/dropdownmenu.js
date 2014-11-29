7fd2e890e3f3cd43843bd31ffd916d5f87028f65
dabolink <dabolink@uvic.ca>
2014-11-28 17:30:38 -0800

    added ping-drop menu and more pings
    have to resize omw ping still and add other 2 pings (other resizing
    still needed as well

--- Javascript/dropdownmenu.js
+++ Javascript/dropdownmenu.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     $('#wardDrop').click(function () {
-        $("#sub-menu1").toggle('wiggle');
-        $('#sub-menu2').hide('wiggle');
-        $('#sub-menu3').hide('wiggle');
+        $("#sub-wardDrop").toggle('wiggle');
+    });
+    $('#pingDrop').click(function () {
+        $("#sub-pingDrop").toggle('wiggle');
     });
 });
 
-function hideDropdown(){
-    $("#sub-menu1").toggle('wiggle');
+function hideDropdown(s){
+    $("#sub-" + s).toggle('wiggle');
 }
--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -17,40 +17,53 @@ img.src = "images/Map.jpg";
 
 function setFunction(setTo){
     function outlineBtn(s) {
-        document.getElementById("lineBtn").style.border = "0px"
-        document.getElementById("refreshBtn").style.border = "0px"
-        document.getElementById("dangerBtn").style.border = "0px"
-        document.getElementById("wardDrop").style.border = "0px"
-        document.getElementById(s).style.border="2px solid #FF0000"
+        document.getElementById("lineBtn").style.border = "2px";
+        document.getElementById("refreshBtn").style.border = "2px";
+        document.getElementById("pingDrop").style.border = "2px";
+        document.getElementById("wardDrop").style.border = "2px";
+        document.getElementById(s).style.border="2px solid #FF0000";
     }
     if(setTo == "pinkWard"){
         console.log("pinkWard set");
-        drawType = 'pinkWard'
-        document.getElementById("wardDrop").setAttribute("src","images/pinkward.jpg")
-        outlineBtn("wardDrop")
-        hideDropdown()
+        drawType = 'pinkWard';
+        document.getElementById("wardDrop").setAttribute("src","images/pinkward.jpg");
+        outlineBtn("wardDrop");
+        hideDropdown("wardDrop");
         resetCoords()
     }
 
     else if(setTo == "greenWard"){
         console.log("greenWard set");
-        drawType = 'greenWard'
-        document.getElementById("wardDrop").setAttribute("src","images/greenward.jpg")
-        outlineBtn("wardDrop")
-        hideDropdown()
+        drawType = 'greenWard';
+        document.getElementById("wardDrop").setAttribute("src","images/greenward.jpg");
+        outlineBtn("wardDrop");
+        hideDropdown("wardDrop");
         resetCoords()
     }
     else if(setTo == "line"){
-        console.log("line set")
+        console.log("line set");
         drawType = 'line';
         outlineBtn("lineBtn");
         resetCoords()
     }
-    else if(setTo == "danger"){
-        console.log("danger ping set")
+    else if(setTo == "dangerPing"){
+        console.log("danger ping set");
         drawType = 'pingDanger';
-        outlineBtn("dangerBtn")
+        document.getElementById("pingDrop").setAttribute("src","images/pingDanger.jpg");
+        outlineBtn("pingDrop");
+        hideDropdown("pingDrop");
         resetCoords();
+    }
+    else if(setTo == "OMWPing"){
+        console.log("OMW ping set");
+        drawType = 'pingOMW';
+        outlineBtn("pingDrop");
+        hideDropdown("pingDrop");
+        document.getElementById("pingDrop").setAttribute("src","images/pingOMW.jpg");
+        resetCoords();
+    }
+    else{
+        console.log("O.o");
     }
 }
 
@@ -58,21 +71,32 @@ myCanvas.addEventListener("mousedown", getPosition, false);
 
 function getPosition(event)
 {
-    if(drawType == "pinkWard"){
+    //draws Image on Canvas centered on mouse click
+    function drawPing(ping) {
+        ctx.drawImage(ping,event.x- c.offsetLeft - ping.width/2,event.y- c.offsetTop - ping.height/2);
+    }
+
+    //draws images depending on drawType set
+    if(drawType == "pinkWard") {
         var ward = new Image();
-        ward.src = "images/pinkward.jpg"
-        ctx.drawImage(ward,event.x- c.offsetLeft - ward.width/2,event.y- c.offsetTop - ward.height/2)
+        ward.src = "images/pinkward.jpg";
+        ctx.drawImage(ward, event.x - c.offsetLeft - ward.width / 2, event.y - c.offsetTop - ward.height / 2);
     }
     else if(drawType == "greenWard"){
         var ward = new Image();
-        ward.src = "images/greenward.jpg"
-        ctx.drawImage(ward,event.x- c.offsetLeft - ward.width/2,event.y- c.offsetTop - ward.height/2)
+        ward.src = "images/greenward.jpg";
+        ctx.drawImage(ward,event.x- c.offsetLeft - ward.width/2,event.y- c.offsetTop - ward.height/2);
     }
     else if(drawType == "pingDanger"){
         var danger = new Image();
         danger.src = "images/ping-danger.jpg";
-        ctx.drawImage(danger,event.x- c.offsetLeft - danger.width/2,event.y- c.offsetTop - danger.height/2)
-        console.log("danger!")
+        drawPing(danger)
+        console.log("danger!");
+    }
+    else if(drawType == "pingOMW"){
+        var OMW = new Image();
+        OMW.src = "images/ping-omw.jpg";
+        drawPing(OMW)
     }
     else if(coords[0] == -1 && coords[1] == -1 && drawType == "line") {
         coords[0] = event.x;
@@ -80,15 +104,15 @@ function getPosition(event)
     }
     else{
         if(drawType == "line"){
-            ctx.beginPath()
+            ctx.beginPath();
             ctx.moveTo(coords[0],coords[1]);
             ctx.lineTo(event.x - c.offsetLeft, event.y - c.offsetTop);
-            ctx.strokeStyle = color
-            ctx.stroke()
-            console.log("line from " + coords[0] + ":" + coords[1] + " to " + event.x + ":"  + event.y)
+            ctx.strokeStyle = color;
+            ctx.stroke();
+            console.log("line from " + coords[0] + ":" + coords[1] + " to " + event.x + ":"  + event.y);
         }
 
-        resetCoords()
+        resetCoords();
     }
 }
 
